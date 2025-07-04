float32 precision bug fixed in RJ sampler. Other minor fixes. RJ case running more convincingly now. Need to add the emission rate matching approach.

dev_scripts/atomized_sources.py
@@ -38,21 +38,34 @@ from openmcmc.sampler.metropolis_hastings import ManifoldMALA
 from openmcmc.mcmc import MCMC
 from pyelq.component.source_model import SourceModelParameter, ScreenedManifoldMALA, SourceReversibleJump, NullSampler
 
+from pyelq.coordinate_system import LLA, ENU
+
 """"Generate the data"""
 
 model = generate_pyelq_test_model()
 model.n_iter = 20
 model.initialise()
 
+# extract real source locations and emission rates
+real_locations = np.concatenate((
+    np.atleast_2d(model.components["source"].dispersion_model.source_map.location.east),
+    np.atleast_2d(model.components["source"].dispersion_model.source_map.location.north),
+    np.atleast_2d(model.components["source"].dispersion_model.source_map.location.up)
+), axis=0)
+real_emission_rates = np.array([[15.0], [10.0]])
+num_real_sources = real_locations.shape[1]
+# TODO (03/07/25): make more robust
+
 """Run the MCMC for the original case"""
 
 model.to_mcmc()
+original_state = deepcopy(model.mcmc.state)
 model.run_mcmc()
 model.from_mcmc()
 
 """
 Configure the data likelihood.
-Test that the compilcation of the likelihood etc. is working.
+Test that the compilation of the likelihood etc. is working.
 """
 
 # create test state
@@ -61,15 +74,31 @@ state = {}
 # control max number of sources
 model.components["source"].n_sources_max = 10
 
+# choose the starting sources
+if False:
+    start_locations = real_locations
+else:
+    start_locations = np.random.uniform(
+        low=np.array([[0], [0], [0]]), high=np.array([[30], [30], [5]]), size=(3, real_locations.shape[1])
+    )
+
 # populate sources
 for i in range(model.components["source"].n_sources_max):
-    state["z" + str(i)] = jnp.array(
-        np.random.uniform(low=np.array([[0], [0], [0]]), high=np.array([[30], [30], [5]]), size=(3, 1))
-    )
+    if i < num_real_sources:
+        state["z" + str(i)] = jnp.array(
+            np.array([[start_locations[0, i]], [start_locations[1, i]], [start_locations[2, i]]])
+        )
+    else:
+        state["z" + str(i)] = jnp.array(
+            np.random.uniform(low=np.array([[0], [0], [0]]), high=np.array([[30], [30], [5]]), size=(3, 1))
+        )
 
 # populate emission rates
 for i in range(model.components["source"].n_sources_max):
-    state["s" + str(i)] = jnp.ones(shape=(1, 1))
+    if i < num_real_sources:
+        state["s" + str(i)] = jnp.array([[real_emission_rates[i, 0]]])
+    else:
+        state["s" + str(i)] = jnp.zeros(shape=(1, 1))
 
 # populate source on/off indicator
 state["q"] = jnp.zeros(shape=(model.components["source"].n_sources_max, 1))
@@ -80,10 +109,13 @@ for i in range(model.components["source"].n_sources_max):
         state["q"] = state["q"].at[i].set(0)
 state["n_src"] = int(jnp.sum(state["q"]))
 
+# convert the generated data to jnp
+state["y"] = jnp.array(original_state["y"])
+
 # populate coupling matrix
-n_data = 500
+n_data = state["y"].shape[0]
 for i in range(model.components["source"].n_sources_max):
-    state["A" + str(i)] = jnp.ones(shape=(n_data, 1))
+    state["A" + str(i)] = jnp.zeros(shape=(n_data, 1))
 
 # create predictor object
 form_dict = {"s" + str(i): "A" + str(i) for i in range(model.components["source"].n_sources_max)}
@@ -96,13 +128,13 @@ source_parameter = SourceModelParameter(
     n_sources_max=model.components["source"].n_sources_max,
 )
 
+# get the coupling columns corresponding to current locations
 test_array, _ = source_parameter.predictor(state)
 for i in range(model.components["source"].n_sources_max):
     state = source_parameter.update_prefactors(state, update_index=i)
-state["y"], _ = source_parameter.predictor(state)
 
 # other params in state
-state["Q"] = (10000.0) * jnp.eye(state["y"].size) # measurement error precision matrix
+state["Q"] = (100.0) * jnp.eye(state["y"].size) # measurement error precision matrix
 state["rho"] = np.array([2.0])  # Poisson rate for the number of sources
 
 # flag for jit compilation
@@ -160,7 +192,7 @@ mdl.response = {"y": "mean"}
 
 sampler_list = []
 for i in range(model.components["source"].n_sources_max):
-    sampler_list.append(ScreenedManifoldMALA("z" + str(i), mdl, step=0.2, parameter_index=i))
+    sampler_list.append(ScreenedManifoldMALA("z" + str(i), mdl, step=0.5, parameter_index=i))
     sampler_list.append(NormalNormal("s" + str(i), mdl))
 sampler_list.append(SourceReversibleJump(
     "n_src", mdl, step=np.array([1.0], ndmin=2),
@@ -170,7 +202,7 @@ sampler_list.append(SourceReversibleJump(
 sampler_list.append(NullSampler("q", mdl))
 
 initial_state = deepcopy(state)
-mcmc = MCMC(initial_state, sampler_list, model=mdl, n_burn=10, n_iter=500)
+mcmc = MCMC(initial_state, sampler_list, model=mdl, n_burn=250, n_iter=250)
 mcmc.run_mcmc()
 
 # NOTE: scaled identity added to the precision matrix in the mMALA sampler, to make it more stable. Should introduce
@@ -182,26 +214,31 @@ Make some plots of the results (both cases).
 
 # plot the fit to the data
 fig = go.Figure()
-y_mean = mcmc.store["y"].mean(axis=1)
-y_std = mcmc.store["y"].std(axis=1)
-fig.add_trace(
-    go.Scatter(
-        x=np.arange(initial_state["y"].shape[0]),
-        y=initial_state["y"][:, 0],
-        mode="lines",
-        name="Observed data",
-        line=dict(color='red')
+data_shape = (len(model.sensor_object), model.sensor_object["Beam sensor 0"].nof_observations)
+y_mean = np.reshape(mcmc.store["y"].mean(axis=1), data_shape).T
+y_std = np.reshape(mcmc.store["y"].std(axis=1), data_shape).T
+y_data = np.reshape(initial_state["y"][:, 0], data_shape).T
+k = 0
+for sensor_key, sensor in model.sensor_object.items():
+    fig.add_trace(
+        go.Scatter(
+            x=sensor.time,
+            y=y_data[:, k],
+            mode="markers",
+            name="Observed data",
+            marker=dict(color=model.sensor_object.color_map[k]),
+        )
     )
-)
-fig.add_trace(
-    go.Scatter(
-        x=np.arange(mcmc.store["y"].shape[0]),
-        y=y_mean,
-        mode="lines",
-        name="Mean fit",
-        line=dict(color='blue')
+    fig.add_trace(
+        go.Scatter(
+            x=sensor.time,
+            y=y_mean[:, k],
+            mode="lines",
+            name="Mean fit",
+            line=dict(color=model.sensor_object.color_map[k])
+        )
     )
-)
+    k += 1
 fig.show()
 
 # plot the traces of the sources
@@ -212,18 +249,37 @@ for i in range(model.components["source"].n_sources_max):
     emission_name = "s" + str(i)
     on_index = mcmc.store["q"][i, :] == 1
     location_series = mcmc.store[location_name][:, on_index]
+    enu_object = ENU(east=location_series[0, :], north=location_series[1, :], up=location_series[2, :],
+                     ref_latitude=0.0, ref_longitude=0.0, ref_altitude=0.0)
+    lla_object = enu_object.to_lla()
     emission_series = mcmc.store[emission_name][:, on_index]
     fig.add_trace(
-        go.Scatter(
-            x=location_series[0, :],
-            y=location_series[1, :],
+            go.Scattermap(
+                mode="markers",
+                lat=np.array(lla_object.latitude),
+                lon=np.array(lla_object.longitude),
+                marker=dict(
+                    size=10,
+                    color=emission_series[0, :],
+                    coloraxis="coloraxis",
+                ),
+                showlegend=False
+            )
+        )
+for i in range(real_locations.shape[1]):
+    enu_object = ENU(east=real_locations[0, i], north=real_locations[1, i], up=0.0,
+                     ref_latitude=0.0, ref_longitude=0.0, ref_altitude=0.0)
+    lla_object = enu_object.to_lla()
+    fig.add_trace(
+        go.Scattermap(
             mode="markers",
+            lat=np.array(lla_object.latitude),
+            lon=np.array(lla_object.longitude),
             marker=dict(
                 size=10,
-                color=emission_series[0, :],
-                coloraxis="coloraxis",
+                color="black"
             ),
-            showlegend=False
+            name=f"Real source {i+1}"
         )
     )
 fig.update_layout(coloraxis = {'colorscale':'jet'})

dev_scripts/helper_functions.py
@@ -80,8 +80,8 @@ def make_meteorology(time_points: pd.array) -> Meteorology:
     nof_observations = time_points.size
     met_object = Meteorology()
     met_object.time = time_points
-    met_object.wind_direction = np.linspace(0.0, 90.0, nof_observations) + np.random.normal(loc=0.0, scale=0.1, size=nof_observations)
-    met_object.wind_speed = 4.0 * np.ones_like(met_object.wind_direction) + np.random.normal(loc=0.0, scale=0.1, size=nof_observations)
+    met_object.wind_direction = np.linspace(0.0, 360.0, nof_observations) + np.random.normal(loc=0.0, scale=0.01, size=nof_observations)
+    met_object.wind_speed = 4.0 * np.ones_like(met_object.wind_direction) + np.random.normal(loc=0.0, scale=0.01, size=nof_observations)
     met_object.calculate_uv_from_wind_speed_direction()
     met_object.temperature = (273.1 + 15.0) * np.ones_like(met_object.wind_direction)
     met_object.pressure = 101.325 * np.ones_like(met_object.wind_direction)
@@ -109,7 +109,7 @@ def make_dispersion_model(
     ) -> GaussianPlume:
     """Create the dispersion model from the previously-specified source map & sensor configuration."""
     dispersion_model = GaussianPlume(source_map=deepcopy(source_map))
-    true_emission_rates = np.array([[15], [10]])
+    true_emission_rates = np.array([[15.0], [10.0]])
     for current_sensor in sensor_group.values():
         coupling_matrix = dispersion_model.compute_coupling(sensor_object=current_sensor, meteorology_object=met_object,
                                                             gas_object=gas_object, output_stacked=False, run_interpolation=False)
@@ -119,7 +119,7 @@ def make_dispersion_model(
     return dispersion_model, sensor_group
 
 
-def preprocess_data(sensor_group: SensorGroup, met_object: Meteorology, smoothing_period: int = 20) -> Preprocessor:
+def preprocess_data(sensor_group: SensorGroup, met_object: Meteorology, smoothing_period: int = 120) -> Preprocessor:
     analysis_time_range = [datetime.datetime(2024, 1, 1, 8, 0, 0), datetime.datetime(2024, 1, 1, 12, 0, 0)]
     time_bin_edges = pd.array(pd.date_range(analysis_time_range[0], analysis_time_range[1], freq=f'{smoothing_period}s'), dtype='datetime64[ns]')
     prepocessor_object = Preprocessor(time_bin_edges=time_bin_edges, sensor_object=sensor_group, met_object=met_object,
@@ -175,8 +175,8 @@ def generate_pyelq_test_model(
     met_object = make_meteorology(time_axis)
     source_map, site_limits = make_source_map(nof_sources=nof_sources)
     gas_object = CH4()
-    prp_object = preprocess_data(sensor_group=sensor_group, met_object=met_object)
     dispersion_model, sensor_group = make_dispersion_model(source_map, sensor_group, met_object, gas_object)
+    prp_object = preprocess_data(sensor_group=sensor_group, met_object=met_object)
     source_model, background_model, offset_model, error_model = make_default_pyelq_inputs(dispersion_model, site_limits)
     model = ELQModel(
         sensor_object=prp_object.sensor_object,

src/pyelq/component/source_model.py
@@ -892,7 +892,10 @@ class SourceModelParameter(LinearCombination_jax):
         sensor_locations (dict):
 
     """
-    sensor_locations: dict
+    sensor_locations_x: dict
+    sensor_locations_y: dict
+    sensor_locations_z: dict
+    form: dict
     wind_speed: jnp.ndarray
     theta: jnp.ndarray
     wind_turbulence_horizontal: jnp.ndarray
@@ -936,7 +939,9 @@ class SourceModelParameter(LinearCombination_jax):
         """Sub-function for extracting and storing sensor location information."""
         source_map_enu = source_map.location
         # TODO (14/06/24): May need to handle the co-ordinate conversion- assuming ENU for now.
-        self.sensor_locations = {}
+        self.sensor_locations_x = {}
+        self.sensor_locations_y = {}
+        self.sensor_locations_z = {}
         for key, sensor in sensor_object.items():
             if isinstance(sensor, Beam):
                 enu_sensor_array = sensor.make_beam_knots(
@@ -952,7 +957,10 @@ class SourceModelParameter(LinearCombination_jax):
                     ref_altitude=source_map_enu.ref_altitude,
                 ).to_array()
                 enu_sensor_array = np.atleast_3d(enu_sensor_array)
-            self.sensor_locations[key] = jnp.array(enu_sensor_array)
+            jax_locations = jnp.array(enu_sensor_array)
+            self.sensor_locations_x[key] = jax_locations[:, [0], :]
+            self.sensor_locations_y[key] = jax_locations[:, [1], :]
+            self.sensor_locations_z[key] = jax_locations[:, [2], :]
 
     def extract_meteorology_information(self, meteorology_object):
         """Sub-function for extracting and storing meteorological information."""
@@ -988,15 +996,18 @@ class SourceModelParameter(LinearCombination_jax):
             update_index = list(range(self.n_sources_max))
         else:
             update_index = [update_index]
-        sensor_key_list = list(self.sensor_locations.keys())
+        sensor_key_list = list(self.sensor_locations_x.keys())
         for idx in update_index:
             source_key = "z" + str(idx)
             coupling_key = "A" + str(idx)
+            sensor_coupling_dict = {}
+            source_x = jnp.atleast_3d(state[source_key][[0], :])
+            source_y = jnp.atleast_3d(state[source_key][[1], :])
+            source_z = jnp.atleast_3d(state[source_key][[2], :])
             for sensor_key in sensor_key_list:
-                relative_x = self.sensor_locations[sensor_key][:, [0], :] - jnp.atleast_3d(state[source_key][[0], :])
-                relative_y = self.sensor_locations[sensor_key][:, [1], :] - jnp.atleast_3d(state[source_key][[1], :])
-                sensor_z = self.sensor_locations[sensor_key][:, [2], :]
-                source_z = jnp.atleast_3d(state[source_key][[2], :])
+                relative_x = self.sensor_locations_x[sensor_key] - source_x
+                relative_y = self.sensor_locations_y[sensor_key] - source_y
+                sensor_z = self.sensor_locations_z[sensor_key]
                 coupling_array = compute_coupling_array_jax(
                     sensor_x=relative_x, sensor_y=relative_y, sensor_z=sensor_z, source_z=source_z,
                     wind_speed=self.wind_speed[sensor_key], theta=self.theta[sensor_key],
@@ -1004,7 +1015,10 @@ class SourceModelParameter(LinearCombination_jax):
                     wind_turbulence_vertical=self.wind_turbulence_vertical[sensor_key],
                     gas_density=self.gas_density
                 )
-                state[coupling_key] = jnp.mean(coupling_array, axis=2)
+                sensor_coupling_dict[sensor_key] = jnp.mean(coupling_array, axis=2)
+            state[coupling_key] = jnp.concatenate(
+                [sensor_coupling_dict[key] for key in sensor_key_list], axis=0
+            )
         return state
 
 
@@ -1034,7 +1048,10 @@ class ScreenedManifoldMALA(ManifoldMALA):
 
     def proposal(self, current_state: dict) -> Tuple[dict, np.ndarray, np.ndarray]:
         """Overloaded proposal."""
-        prop_state = deepcopy(current_state)
+        # prop_state = deepcopy(current_state)
+        prop_state = current_state.copy()
+        prop_state[self.param] = jnp.copy(current_state[self.param])
+        # TODO (04/07/25): does this work? And do we also need to copy the coupling?
 
         mu_cr, chol_cr = self._proposal_params(current_state)
         prop_state[self.param] = gmrf.sample_normal(mu_cr, L=chol_cr)
@@ -1101,8 +1118,8 @@ class SourceReversibleJump(ReversibleJump):
 
         prop_state = deepcopy(current_state)
         prop_state[self.param] -= 1
-        ones_loc = jnp.argwhere(prop_state[self.indicator_var] == 1)
-        death_index = int(np.random.choice(np.array(ones_loc).flatten()))
+        ones_loc = jnp.argwhere(prop_state[self.indicator_var].flatten() == 1)
+        death_index = int(np.random.choice(np.array(ones_loc.flatten())))
         prop_state[self.indicator_var] = prop_state[self.indicator_var].at[death_index].set(0)
         # prop_state[self.indicator_var][death_index] = 0
         death_location = "z" + str(death_index)
@@ -1135,12 +1152,19 @@ class SourceReversibleJump(ReversibleJump):
                 logp_pr_loc, _ = self.model[loc].log_p(prop_state)
                 logp_pr += (logp_pr_rate + logp_pr_loc)
             ct += 1
-        for var in self.other_variables:
-            logp_cr_dist, _ = self.model[var].log_p(current_state)
-            logp_cs += logp_cr_dist
-            logp_pr_dist, _ = self.model[var].log_p(prop_state)
-            logp_pr += logp_pr_dist
-        log_accept = logp_pr + logp_cr_g_pr - (logp_cs + logp_pr_g_cr)
+        logp_cs_y, _ = self.model["y"].log_p(current_state)
+        logp_pr_y, _ = self.model["y"].log_p(prop_state)
+        logp_cs_rho, _ = self.model["n_src"].log_p(current_state)
+        logp_pr_rho, _ = self.model["n_src"].log_p(prop_state)
+        likelihood_diff = logp_pr_y - logp_cs_y
+        logp_pr += logp_pr_rho
+        logp_cs += logp_cs_rho
+        # for var in self.other_variables:
+        #     logp_cr_dist, _ = self.model[var].log_p(current_state)
+        #     logp_cs += logp_cr_dist
+        #     logp_pr_dist, _ = self.model[var].log_p(prop_state)
+        #     logp_pr += logp_pr_dist
+        log_accept = likelihood_diff + logp_pr + logp_cr_g_pr - (logp_cs + logp_pr_g_cr)
 
         if self.accept_proposal(log_accept):
             current_state = prop_state